6b347c6ab3b65e24b822fea29a85fd258964a20f
George Soderholm <georgesoderholm@gmail.com>
2020-11-10 13:44:02 -0700

    Remove commented out code

--- app/controllers/passenger_flights_controller.rb
+++ app/controllers/passenger_flights_controller.rb
@@ -1,12 +1,26 @@
 class PassengerFlightsController < ApplicationController
-  def new
-    @passenger = Passenger.find(params[:id]
-  end
+    def new
+      @passenger = Passenger.find(params[:id])
+    end
 
-  def create
-    passenger = Passenger.find(params[:id])
-    passenger_flights = PassengerFlights.find_by(passenger)
-    binding.pry
-    redirect_to "/passengers/#{passenger.id}"
-  end
+    def create
+      airline = Airline.all.first
+      passenger = Passenger.find(params[:id])
+      # binding.pry
+      passenger.flights.create({
+        number: params[:number],
+        date: params[:date],
+        departure_city: params[:departure_city],
+        arrival_city: params[:arrival_city],
+        time: params[:time],
+        airline_id: airline.id
+        })
+      # binding.pry
+      redirect_to "/passengers/#{passenger.id}"
+    end
+
+    private
+    def flight_params
+      params.permit(:number, :date, :departure_city, :arrival_city, :time)
+    end
 end
