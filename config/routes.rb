6d8e86a12c5bc89ecf08801fba993ac063d10f47
George Soderholm <georgesoderholm@gmail.com>
2020-11-10 13:41:34 -0700

    Reformat routes to make sense with the form moving pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flights, only: [:show]
   get '/passengers/:id/flights/new', to: 'passenger_flights#new'
-  post '/passengers/:id/flights/:flight_id', to: 'passenger_flights#create'
+  post '/passengers/:id/flights', to: 'passenger_flights#create'
 
   resources :passengers, only: [:show]
 end
