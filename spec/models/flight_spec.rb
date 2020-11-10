4064774bb98cdfa47d3d281466a21bf173aa31ef
George Soderholm <georgesoderholm@gmail.com>
2020-11-10 13:50:15 -0700

    Add test for a model level test of a method named airline_name

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Flight, type: :model do
     it { should have_many :passenger_flights }
     it { should have_many(:passengers).through :passenger_flights }
   end
+
+  describe 'instance methods' do
+    it '.airline_name' do
+      airline_1 = Airline.create!(name: 'Frontier')
+      flight_3 = airline_1.flights.create!(number: 937, date: "20/11/16", departure_city: 'Denver', arrival_city: 'Montreal', time: '15:30')
+      expect(flight_3.airline_name).to eq(airline_1.name)
+    end
+  end
 end
